6cf13ef2a09e607e19f6d8c0bf819dcb9c8c62e2
ThompsonE830 <csmetho1@ljmu.ac.uk>
2026-03-13 14:27:50 +0000

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -26,7 +26,7 @@ mysqli_query($conn,$update);
 
 // updates database with current reset token
 
-$link = "http://localhost/new-sec-main%20(1)/password_reset.php?token=".$token;
+$link = "http://localhost/CourseWork02(2)/password_reset.php?token=".$token;
 
 // Email details
 $to = $email;
@@ -129,3 +129,4 @@ button:hover {
 </body>
 
 </html>
+
