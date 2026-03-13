ad5cf339653ed46b6d239e0f49c2079efc679ce5
ThompsonE830 <csmetho1@ljmu.ac.uk>
2026-03-13 13:47:12 +0000

    Add files via upload
    Protected against SQL injection

--- forgot_password.php
+++ forgot_password.php
@@ -6,9 +6,12 @@ if (isset($_POST['email'])){
 	
 $email = $_POST['email'];
 // check for an email and saves it.
+$prep_query = $conn->prepare("SELECT * FROM user WHERE email = ?");
+$prep_query->bind_param("s", $email);
+$prep_query->execute();
+// statement pre prepared prevent Injection
 
-$query = "SELECT * FROM user WHERE email='$email'";
-$result = mysqli_query($conn,$query);
+$result = $prep_query->get_result();
 
 // executes query to find email exists
 
@@ -43,13 +46,86 @@ echo"email not found";
 
 ?>
 
-<form method="POST">
+<html>
+<style>
+body {font-family: Arial, Helvetica, sans-serif; width:500px;}
+* {box-sizing: border-box;}
 
-<h2>Forgot Password</h2>
+/* Full-width input fields */
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 15px;
+    margin: 5px 0 22px 0;
+    display: inline-block;
+    border: none;
+    background: #f1f1f1;
+}
 
-<input type="email" name="email">
+input[type=text]:focus, input[type=password]:focus {
+    background-color: #ddd;
+    outline: none;
+}
 
-<button type="submit">Send reset link</button>
+hr {
+    border: 1px solid #f1f1f1;
+    margin-bottom: 25px;
+}
 
+/* Set a style for all buttons */
+button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+    opacity: 0.9;
+}
 
+button:hover {
+    opacity:1;
+}
+
+/* Float cancel and signup buttons and add an equal width */
+.resetpswbtn {
+  float: left;
+  width: 50%;
+}
+
+/* Add padding to container elements */
+.container {
+    padding: 16px;
+}
+
+/* Clear floats */
+.clearfix::after {
+    content: "";
+    clear: both;
+    display: table;
+}
+
+/* Change styles for cancel button and signup button on extra small screens */
+@media screen and (max-width: 300px) {
+	.resetpswbtn{
+       width: 100%;
+    }
+}
+</style>
+
+<body>
+
+<form method="POST" style="border:1px solid #ccc">
+  <div class="container">
+	<a href="home.php">Home</a>
+    <h1>Forgot Password</h1>
+    <p>Please enter your email to reset password</p>
+    <input type="email" placeholder="Enter Email" name="email" required>
+    <div class="clearfix">
+      <button type="submit" class="resetemailbtn">Send Reset Link<button>
+    </div>
+  </div>
 </form>
+
+</body>
+</html>
